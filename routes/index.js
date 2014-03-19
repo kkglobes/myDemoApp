b6d72211f0394fa6134123e2d4ceb68d8d186c26
Katharine Walsh <katharinew@zillow.com>
2014-03-19 10:42:50 -0700

    step 6 Gettin' crazy

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,12 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Tech Deland',
+    page_title: 'Home',
+    seo_description: 'Techonology meetup in downtown Deland, FL for graphic designers, web designers, web developers and photographers',
+   // speakers: '' //delete speakers: '' for speakers to appear
+  });
 };
 
 /* GET About Us. */
