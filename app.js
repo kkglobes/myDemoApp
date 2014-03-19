bff492c2368831ca4d571cdbeb9237ac80f9ea15
Katharine Walsh <katharinew@zillow.com>
2014-03-19 09:58:35 -0700

    step 2 Sweet Routing

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('Hello Express');
+  res.send('<h1>Hello Express</h1>');
 });
+
+app.get('/me', function(req, res){
+  res.send('<h1>Ray Villalobos</h1>');
+});
+
+app.get('/fluent', function(req, res){
+  res.send('<h1>Fluent Conference</h1>');
 
 var server = app.listen(3000, function() {
   console.log('Listening on port %d', server.address().port);
